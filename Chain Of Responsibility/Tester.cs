32a72fb46118dcc77a425219de2b57593158c6c4
Subasish Mohapatra <subasish.mohapatra@hotmail.com>
2016-12-13 13:02:32 +0530

    Added chain of responsibility

---
+++ Chain Of Responsibility/Tester.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chain_Of_Responsibility
+{
+    public class Tester : Role
+    {
+        public override void DoJob(Project project)
+        {
+            Console.WriteLine("Test App");
+            base.DoJob(project);
+        }
+    }
+}
